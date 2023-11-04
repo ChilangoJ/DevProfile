0890f0effd73357338202d8c65ecbb4bd23a0c11
Jay <juliuschilango@gmail.com>
2023-11-04 19:55:23 +0300

    Devslopes 1st app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,13 @@ plugins {
 
 android {
     namespace = "com.example.devprofile"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.devprofile"
         minSdk = 24
         targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
